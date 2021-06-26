34538da74cc64740737c2859b9a9224d459c20b9
ankitdipto <ankitdipto@gmail.com>
2021-06-26 17:03:27 +0530

    moved personal exploration files to my-utilities folder

--- refine.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-dir='/home/ankit/Documents/ProductRecognition/datasets/GP-Test'
-file='/home/ankit/Documents/ProductRecognition/datasets/GP-Test/temp.txt'  
-i=1  
-while read line; do  
-    cp $dir/'images'/$line $dir/'IMAGES'/
-    i=$((i+1)) 
-done < $file  
